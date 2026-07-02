78ba978d503d62a9f8f93855e2c3f948020175f2
LucasCodeur <l.dadamopro@gmail.com>
2026-07-02 09:30:43 -0400

    feat: mariadb works with new volume, maybe have to change

--- srcs/requirements/mariadb/launch_mariadb.sh
+++ srcs/requirements/mariadb/launch_mariadb.sh
@@ -2,7 +2,19 @@
 
 set -e
 
+echo "just before mkdir"
 mkdir -p /run/mysqld
+echo "just chown"
 chown -R mysql:mysql /run/mysqld /var/lib/mysql
 
+echo "before if"
+# if [ ! -d /var/lib/mysql/mysql ]; then
+#     mariadb-install-db \
+#         --user=mysql \
+#         --basedir=/usr \
+#         --datadir=/var/lib/mysql
+# fi
+
+echo "before exec"
 exec mariadbd --user=mysql --console
+
