86b204afe4e44275b06fa8c4ca0c6745f59acf0a
LucasCodeur <l.dadamopro@gmail.com>
2026-07-11 14:26:23 -0400

    backup

--- srcs/requirements/mariadb/launch_mariadb.sh
+++ srcs/requirements/mariadb/launch_mariadb.sh
@@ -2,12 +2,9 @@
 
 set -e
 
-echo "just before mkdir"
 mkdir -p /run/mysqld
-echo "just chown"
 chown -R mysql:mysql /run/mysqld /var/lib/mysql
 
-echo "before if"
 # if [ ! -d /var/lib/mysql/mysql ]; then
 #     mariadb-install-db \
 #         --user=mysql \
@@ -15,6 +12,5 @@ echo "before if"
 #         --datadir=/var/lib/mysql
 # fi
 
-echo "before exec"
 exec mariadbd --user=mysql --console
 
