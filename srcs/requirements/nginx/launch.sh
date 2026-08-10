4c5533d62e27685da79249a243a5c45f438b6dd5
LucasCodeur <l.dadamopro@gmail.com>
2026-08-10 12:23:48 -0400

    feat: certificate tls = secrets

--- srcs/requirements/nginx/launch.sh
+++ srcs/requirements/nginx/launch.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-# touch /etc/nginx/tls/server.key
-# touch /etc/nginx/tls/server.crt
-# cat SERVER_KEY_PATH > /etc/nginx/tls/server.key
-# cat SERVER_CRT_PATH > /etc/nginx/tls/server.crt
-
 nginx -g "daemon off;"
